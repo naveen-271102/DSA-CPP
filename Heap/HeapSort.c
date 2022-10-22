3ec719276b01a8679f00df3372e4354e434007a8
Krishan Kumar <92982687+Crimson-03@users.noreply.github.com>
2022-10-22 15:36:24 +0530

    Delete function for Heap Sort added

--- Heap/HeapSort.c
+++ Heap/HeapSort.c
@@ -13,6 +13,33 @@ void Insert(int H[], int n)
     H[i] = temp;
 }
 
+int Delete(int H[], int n)
+{
+    int i,j,x,temp,val;
+    val = H[1];
+    x = H[n];
+    H[1] = H[n];
+    i = 1;j=i*2;
+
+    while(j<n-1)
+    {
+        if(H[j+1]>H[j])
+            j = j+1;
+        if(H[i]<H[j])
+        {
+            temp = H[i];
+            H[i] = H[j];
+            H[j] = temp;
+            i = j;
+            j = j*2;
+        }
+        else
+            break;
+    }
+    H[n] = val;
+    return val;
+}
+
 int main()
 {
     int H[] = {0,10,20,30,25,5,40,35};
@@ -21,6 +48,9 @@ int main()
     {
         Insert(H,i);
     }
+
+    printf("Deleted value is %d\n", Delete(H,7));
+    printf("Deleted value is %d\n", Delete(H,6));
 
     for(i=1;i<=7;i++)
     {
